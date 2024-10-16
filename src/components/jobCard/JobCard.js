f37de3edf8d4bd2aae6c1c7df0d45e07df13d25c
ChetanPalade <147688599+ChetanPalade@users.noreply.github.com>
2024-10-16 12:58:01 +0530

    Update JobCard.js

--- src/components/jobCard/JobCard.js
+++ src/components/jobCard/JobCard.js
@@ -1,26 +1,3 @@
-// import React from 'react'
-// import {useSwipeable} from 'react-swipeable';
-// import './JobCard.css'
-
-// const JobCard = ({job, onBookmark, onDismiss}) => {
-//    const handlers = useSwipeable({
-//     onSwipedLeft: () => onDismiss(job),
-//     onSwipedRight: () => onBookmark(job),
-//     preventDefaultTouchmoveEvent: true,
-//     trackMouse: true,
-//    });
-
-
-//   return (
-//     <div {...handlers} className='job-card'>
-//       <h3>{job.title}</h3>
-//       <p>{job.company}</p>
-//       <p>{job.location}</p>
-//     </div>
-//   )
-// }
-
-// export default JobCard
 
 import React, { useState } from 'react';
 import { useSwipeable } from 'react-swipeable';
@@ -64,3 +41,26 @@ const JobCard = ({ job, onBookmark, onDismiss }) => {
 };
 
 export default JobCard;
+// import React from 'react'
+// import {useSwipeable} from 'react-swipeable';
+// import './JobCard.css'
+
+// const JobCard = ({job, onBookmark, onDismiss}) => {
+//    const handlers = useSwipeable({
+//     onSwipedLeft: () => onDismiss(job),
+//     onSwipedRight: () => onBookmark(job),
+//     preventDefaultTouchmoveEvent: true,
+//     trackMouse: true,
+//    });
+
+
+//   return (
+//     <div {...handlers} className='job-card'>
+//       <h3>{job.title}</h3>
+//       <p>{job.company}</p>
+//       <p>{job.location}</p>
+//     </div>
+//   )
+// }
+
+// export default JobCard
